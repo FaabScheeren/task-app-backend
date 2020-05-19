c907db845720fd6413fe39fda35a88b69b92aa82
FaabScheeren <f.scheeren@outlook.com>
2020-05-19 10:31:01 +0200

    Solved file upload issue

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,140 @@
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+// const brcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const Task = require("./task");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//       lowercase: true,
+//       validate(value) {
+//         if (!validator.isEmail(value)) {
+//           throw new Error("Email is invalid.");
+//         }
+//       },
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//       validate(value) {
+//         if (value.includes("password")) {
+//           throw new Error(
+//             "Password isn't allowed to contain the word 'password'."
+//           );
+//         }
+//         if (value.length <= 6) {
+//           throw new Error("Password must at least be 7 characters.");
+//         }
+//       },
+//     },
+//     age: {
+//       type: Number,
+//       default: 0,
+//       validate(value) {
+//         if (value < 0) {
+//           throw new Error("Age must be a positive number.");
+//         }
+//       },
+//     },
+//     tokens: [
+//       {
+//         token: {
+//           type: String,
+//           required: true,
+//         },
+//       },
+//     ],
+//   },
+//   {
+//     timestamps: true,
+//   },
+//   {
+//     avatar: {
+//       type: Buffer,
+//     },
+//   }
+// );
+
+// userSchema.virtual("tasks", {
+//   ref: "Task",
+//   localField: "_id",
+//   foreignField: "owner",
+// });
+
+// userSchema.methods.toJSON = function () {
+//   const user = this;
+//   const userObject = user.toObject();
+
+//   delete userObject.password;
+//   delete userObject.tokens;
+//   delete userObject.avatar;
+
+//   return userObject;
+// };
+
+// userSchema.methods.generateAuthToken = async function () {
+//   const user = this;
+
+//   const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+
+//   user.tokens = user.tokens.concat({ token });
+//   await user.save();
+//   return token;
+// };
+
+// userSchema.statics.findByCredentials = async (email, password) => {
+//   const user = await User.findOne({ email });
+
+//   if (!user) {
+//     throw new Error("Unable to login");
+//   }
+
+//   const isMatch = await brcrypt.compare(password, user.password);
+
+//   if (!isMatch) {
+//     throw new Error("Unable to login");
+//   }
+
+//   return user;
+// };
+
+// // Mongodb middlewares
+// // Hash the plain text password before saving
+// userSchema.pre("save", async function (next) {
+//   const user = this;
+
+//   if (user.isModified("password")) {
+//     user.password = await brcrypt.hash(user.password, 8);
+//   }
+
+//   next();
+// });
+
+// // Delete user tasks when user is removed
+// userSchema.pre("remove", async function (next) {
+//   const user = this;
+//   await Task.deleteMany({ owner: user._id });
+//   next();
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+// module.exports = User;
+
 const mongoose = require("mongoose");
 const validator = require("validator");
-const brcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
@@ -13,28 +147,24 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
       unique: true,
+      required: true,
       trim: true,
       lowercase: true,
       validate(value) {
         if (!validator.isEmail(value)) {
-          throw new Error("Email is invalid.");
+          throw new Error("Email is invalid");
         }
       },
     },
     password: {
       type: String,
       required: true,
+      minlength: 7,
       trim: true,
       validate(value) {
-        if (value.includes("password")) {
-          throw new Error(
-            "Password isn't allowed to contain the word 'password'."
-          );
-        }
-        if (value.length <= 6) {
-          throw new Error("Password must at least be 7 characters.");
+        if (value.toLowerCase().includes("password")) {
+          throw new Error('Password cannot contain "password"');
         }
       },
     },
@@ -43,7 +173,7 @@ const userSchema = new mongoose.Schema(
       default: 0,
       validate(value) {
         if (value < 0) {
-          throw new Error("Age must be a positive number.");
+          throw new Error("Age must be a postive number");
         }
       },
     },
@@ -55,14 +185,12 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
-  },
-  {
-    timestamps: true,
-  },
-  {
     avatar: {
       type: Buffer,
     },
+  },
+  {
+    timestamps: true,
   }
 );
 
@@ -78,17 +206,18 @@ userSchema.methods.toJSON = function () {
 
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
 
   return userObject;
 };
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-
   const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
+
   return token;
 };
 
@@ -99,7 +228,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     throw new Error("Unable to login");
   }
 
-  const isMatch = await brcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
 
   if (!isMatch) {
     throw new Error("Unable to login");
@@ -108,13 +237,12 @@ userSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
-// Mongodb middlewares
 // Hash the plain text password before saving
 userSchema.pre("save", async function (next) {
   const user = this;
 
   if (user.isModified("password")) {
-    user.password = await brcrypt.hash(user.password, 8);
+    user.password = await bcrypt.hash(user.password, 8);
   }
 
   next();
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const router = new express.Router();
 const auth = require("../middleware/auth");
@@ -103,14 +104,49 @@ router.delete("/users/me", auth, async (req, res) => {
   }
 });
 
+// const upload = multer({
+//   limits: {
+//     fileSize: 1000000,
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+//       return cb(new Error("Please upload a jpg, jpeg or png."));
+//     }
+//     cb(undefined, true);
+//   },
+// });
+
+// router.post(
+//   "/users/me/avatar",
+//   auth,
+//   upload.single("avatar"),
+//   async (req, res) => {
+//     const buffer = await sharp(req.file.buffer)
+//       .resize({ width: 250, height: 250 })
+//       .png.toBuffer();
+
+//     // console.log("buffer", req.file.buffer);
+//     // console.log("user", req.user);
+//     req.user.avatar = buffer;
+//     // console.log("user after", req.user);
+//     // console.log("user after", req.user.avatar);
+//     await req.user.save();
+//     res.send();
+//   },
+//   (error, req, res, next) => {
+//     res.status(400).send({ error: error.message });
+//   }
+// );
+
 const upload = multer({
   limits: {
     fileSize: 1000000,
   },
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error("Please upload a jpg, jpeg or png."));
+      return cb(new Error("Please upload an image"));
     }
+
     cb(undefined, true);
   },
 });
@@ -120,11 +156,11 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    // console.log("buffer", req.file.buffer);
-    // console.log("user", req.user);
-    req.user.avatar = req.file.buffer;
-    // console.log("user after", req.user);
-    // console.log("user after", req.user.avatar);
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -132,5 +168,20 @@ router.post(
     res.status(400).send({ error: error.message });
   }
 );
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send();
+  }
+});
 
 module.exports = router;
